68e004b7acab505ecfd1704fd1064ade0a0d8d69
Aveev Bair <aveevb123@gmail.com>
2025-06-21 09:12:45 +0300

    chore: update nextConfig to deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/o-complex-test',
+  output: 'export',
 };
 
 export default nextConfig;
